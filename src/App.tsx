da80bac19f84401feedf7bb913e9d5783123e592
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-03 22:23:59 +0800

    add about text

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import Home from './components/home';
 import Navbar from './components/navbar';
+import About from './components/AboutMe';
 import './App.css';
 
 const anchors = ['Home', 'About-me', 'thirdPage'];
@@ -16,14 +17,12 @@ const App = () => {
         anchors={anchors}
         navigation
         navigationTooltips={navigation}
-        sectionsColor={['#282c34', '#ff5f45', '#0798ec']}
+        sectionsColor={['#282c34', '#191d27', '#0798ec']}
         render={({state, fullpageApi}) => {
           return (
             <div>
               <Home />
-              <div className="section">
-                <p>Section 2</p>
-              </div>
+              <About />
             </div>
           );
         }}
---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './about.sass';
+
+const About = () => {
+  return (
+    <div className="section">
+      <div id="about">
+        <h1>About me</h1>
+        <p>
+          台南高工的某位廢物，不會寫程式成績也爛的可以，不會Python也不會C#每天被Young嘲諷的可憐傢伙。
+        </p>
+        <div id="contact">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
