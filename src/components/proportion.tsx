e55f5a016acfb1e7885a495d7ddb1441a56c0ea2
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-05 17:28:23 +0800

    add repo languages api

--- src/components/proportion.tsx
+++ src/components/proportion.tsx
@@ -3,24 +3,39 @@ import axios from 'axios';
 
 const Proportion: FC<{url: string}> = ({url}) => {
   const [lan, setLan] = useState<any>([]);
+  let [codeNumber, setCodeNumber] = useState(0);
   useEffect(() => {
     axios.get(`https://api.github.com/repos/${url}/languages`)
         .then((data) => {
           const lanList:Array<any> = [];
           for (const key in data.data) {
             if (key) {
-              const lanData = {};
-              lanData[`${key}`] = data.data[key];
-              // console.log(lanData);
-              lanList.push(lanData);
+              setCodeNumber(codeNumber += data.data[key]);
+              lanList.push([key, data.data[key]]);
             }
           }
           setLan(lanList);
+          console.log(url, lanList, codeNumber);
         });
   }, []);
+
   return (
     <div className="proportion">
-      <div></div>
+      <div>
+        {lan.map((lanData) => {
+          return (
+            <div
+              className={lanData[0]}
+              key={lanData[0]}
+              style={{
+                width: `${(codeNumber / lanData[1]) * 100}%`,
+                height: '100%',
+              }}
+            >
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
