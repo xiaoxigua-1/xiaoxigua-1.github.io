5a1fc945ff16ab1e851a330964fa00e14c6ffccc
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-05 19:59:43 +0800

    add languaes sass

--- src/components/proportion.tsx
+++ src/components/proportion.tsx
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 const Proportion: FC<{url: string}> = ({url}) => {
   const [lan, setLan] = useState<any>([]);
-  let [codeNumber, setCodeNumber] = useState(0);
+  const [codeNumber, setCodeNumber] = useState(0);
   useEffect(() => {
     axios.get(`https://api.github.com/repos/${url}/languages`)
         .then((data) => {
           const lanList:Array<any> = [];
           for (const key in data.data) {
             if (key) {
-              setCodeNumber(codeNumber += data.data[key]);
+              setCodeNumber(codeNumber + data.data[key]);
               lanList.push([key, data.data[key]]);
             }
           }
@@ -18,24 +18,23 @@ const Proportion: FC<{url: string}> = ({url}) => {
           console.log(url, lanList, codeNumber);
         });
   }, []);
-
   return (
     <div className="proportion">
-      <div>
-        {lan.map((lanData) => {
-          return (
-            <div
-              className={lanData[0]}
-              key={lanData[0]}
-              style={{
-                width: `${(codeNumber / lanData[1]) * 100}%`,
-                height: '100%',
-              }}
-            >
-            </div>
-          );
-        })}
-      </div>
+      {lan.map((lanData) => {
+        return (
+          <div
+            className={
+              (lanData[0] === 'C#') ? 'CS' : lanData[0] + ' languages'
+            }
+            key={lanData[0]}
+            style={{
+              width: `${(codeNumber / lanData[1]) * 100}%`,
+              height: '120%',
+            }}
+          >
+          </div>
+        );
+      })}
     </div>
   );
 };
